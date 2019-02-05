3746739ae0b9a6ba21ec510b80e6e81ecac1f951
Екатерина <krasnovaEA95@yandex.ru>
2019-02-05 13:51:42 -0500

    MyApplicationListener

---
+++ src/main/java/MyApplicationListener.java
@@ -0,0 +1,43 @@
+import javax.lang.model.element.PackageElement;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class MyApplicationListener implements ServletContextListener {
+
+    private EntityManager em;
+    private EntityManagerFactory factory;
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        factory = Persistence.createEntityManagerFactory("TestPersistenceUnit");
+        em = factory.createEntityManager();
+
+        event.getServletContext().setAttribute("em", em);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        if (em != null){
+            em.close();
+        }
+        if (factory != null){
+            factory.close();
+        }
+    }
+
+    private void unitUsers(){
+        em.getTransaction().begin();
+        try {
+            new UserDAO(em).inviteUser("test-login", "test-password", "test-email");
+            em.getTransaction().rollback();
+        } catch (Exception e){
+            em.getTransaction().rollback();
+            throw new RuntimeException(e);
+        }
+    }
+}
