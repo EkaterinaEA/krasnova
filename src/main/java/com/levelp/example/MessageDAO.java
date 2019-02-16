e24e15594b964697b455b2e9f01571a11ac0c621
Екатерина <krasnovaEA95@yandex.ru>
2019-02-16 14:14:03 -0500

    add Transactional

--- src/main/java/com/levelp/example/MessageDAO.java
+++ src/main/java/com/levelp/example/MessageDAO.java
@@ -1,6 +1,5 @@
 package com.levelp.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
